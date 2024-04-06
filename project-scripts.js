aceda858987addedacf41ea7e11d11ddc3917517
Jose Lee <andrelee089@gmail.com>
2024-04-05 21:52:05 -0700

    starter code + assets

---
+++ project-scripts.js
@@ -0,0 +1,43 @@
+/*Car dataset */
+let cars = [
+    {
+        'manufacturer': 'Lexus', 
+        'model': 'LC 500', 
+        'year': 2024, 
+        'color': 'carbon', 
+        'top-speed': 168,
+        'engine': {
+            'displacement': 5.0, 
+            'horsepower': 471, 
+            'max-torque': 398,
+            'type': 'gasoline V8'
+        }
+    },
+    {
+        'manufacturer': 'Porsche',
+        'model': '911 GT3 RS',
+        'year': 2024,
+        'color': 'green',
+        'top-speed': 184,
+        'engine': {
+            'displacement': 4.0,
+            'horsepower': 518,
+            'max-torque': 324,
+            'type': 'gasoline flat-six'
+            
+        }
+    },
+    {
+        'manufacturer': 'Audi',
+        'model': 'R8',
+        'year': 2024,
+        'color': 'yellow',
+        'top-speed': 201,
+        'engine': {
+            'displacement': 5.2,
+            'horsepower': 532,
+            'max-torque': 398,
+            'type': 'gasoline V10'
+        }
+    }
+];
